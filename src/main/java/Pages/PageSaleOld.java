1cdc7ef0aef37e7d319be68a3a8a081251ecd706
truyendinhthu <89843602+truyendinhthu@users.noreply.github.com>
2022-11-02 16:37:26 +0700

    part_2

--- src/main/java/Pages/PageSaleOld.java
+++ src/main/java/Pages/PageSaleOld.java
@@ -1,18 +1,21 @@
 package Pages;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Sleeper;
 
 public class PageSaleOld extends Page {
-//	By btnCancel= By.className("introjs-button introjs-nextbutton introjs-fullbutton");
-	By btn = By.cssSelector(".introjs-skipbutton");
+	By btnNext= By.xpath("//a[@class='introjs-skipbutton']");
 	By txtSearchProductInput = By.id("productSearchInput");
 	By txtSearchCustomerInput = By.id("customerSearchInput");
-	By txtDeliveryPartnerSearchInput = By.id("deliveryPartnerSearchInput");
 	By btnPay = By.id("saveTransactionDelivery");
-	
 	By btnNewSale = By.className("switch-newpos");
 	public PageSaleOld(WebDriver dr) {
 		super(dr);
@@ -20,19 +23,29 @@ public class PageSaleOld extends Page {
 	}
 	public void cancel() {
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		WebElement next = driverWeb.findElement(btnNext);
 		
-		System.out.println(driverWeb.findElement(btn).getText());
-		driverWeb.findElement(btn).click();
 		
+		JavascriptExecutor executor = (JavascriptExecutor)driverWeb;
+		executor.executeScript("arguments[0].click();", next); 
+
 	}
 	public void createOrder(String productName, String customerName) {
 		driverWeb.findElement(txtSearchProductInput).sendKeys(productName);
 		driverWeb.findElement(txtSearchProductInput).sendKeys(Keys.ENTER);
-		driverWeb.findElement(txtSearchCustomerInput).sendKeys(customerName);
-		driverWeb.findElement(txtSearchCustomerInput).sendKeys(Keys.ENTER);
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		driverWeb.findElement(txtSearchCustomerInput).sendKeys(customerName);	
+		WebElement next = driverWeb.findElement(By.xpath("//li[@val='Truyền']"));	
+		JavascriptExecutor executor = (JavascriptExecutor)driverWeb;
+		executor.executeScript("arguments[0].click();", next); 
+		
 	}
 }
